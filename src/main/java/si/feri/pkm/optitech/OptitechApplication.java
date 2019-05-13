2f3488f445e3133640b21dcc4a4f745e0e50a784
uzagoranski <urozag@gmail.com>
2019-05-13 12:07:50 +0200

    Initial commit

---
+++ src/main/java/si/feri/pkm/optitech/OptitechApplication.java
@@ -0,0 +1,14 @@
+package si.feri.pkm.optitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptitechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptitechApplication.class, args);
+    }
+
+}
+i
---
+++ src/test/java/si/feri/pkm/optitech/OptitechApplicationTests.java
@@ -0,0 +1,16 @@
+package si.feri.pkm.optitech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OptitechApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
