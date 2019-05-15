ac5bc1e487aa5b17e4a28e3959c218204696256b
Primoz Stopar <primoz.stopar@student.um.si>
2019-05-15 13:44:23 +0200

    Dodana povezava na bazo in branje avtomobilov iz baze.

---
+++ src/main/java/si/feri/pkm/optitech/Database/SQLDatabaseConnection.java
@@ -0,0 +1,40 @@
+package si.feri.pkm.optitech.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class SQLDatabaseConnection {
+   private static String connectionUrl =
+            "jdbc:sqlserver://DESKTOP-U925N8J:1433;databaseName=OptiTech;integratedSecurity=true";
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    public static ArrayList<String> getInsertedVehicles() {
+
+
+        ResultSet resultSet;
+        ArrayList<String> avti = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(connectionUrl);
+             Statement statement = connection.createStatement()) {
+
+            // Create and execute a SELECT SQL statement.
+            String selectSql = "SELECT vehicleId ,vin, vehicleTitle, vehicleName, regNumber, carModelId, fuelTypeId, engineSize, enginePower, enginePowerHP FROM OptiTech.biz.Vehicles  WHERE vehicleTitle != 'Null' AND vehicleName != 'Null' AND vin != 'Null' AND regNumber != 'Null' AND vehicleTitle != 'Car' AND enginePowerHP != 0;";
+            resultSet = statement.executeQuery(selectSql);
+
+
+            // Print results from select statement
+            while (resultSet.next()) {
+                avti.add(resultSet.getString(4));
+//                System.out.println(resultSet.getString(4));
+
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    return avti;
+    }
+}
--- src/main/java/si/feri/pkm/optitech/controller/MainController.java
+++ src/main/java/si/feri/pkm/optitech/controller/MainController.java
@@ -4,15 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import si.feri.pkm.optitech.Database.SQLDatabaseConnection;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 @Controller
 public class MainController {
 
     @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
     public String index(Model model) {
+
+
+        System.out.println();
+
         return "index";
     }
 
     @RequestMapping(value = {"/carsList"}, method = RequestMethod.GET)
-    public String seznamVozil(Model model) {return "carsList";}
+    public String seznamVozil(Model model) {
+        ArrayList<String> vehicles = SQLDatabaseConnection.getInsertedVehicles();
+
+        for(int i =0; i<vehicles.size();i++){
+            System.out.println(vehicles.get(i));
+        }
+
+        return "carsList";
+
+    }
 }
