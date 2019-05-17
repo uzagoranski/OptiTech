2ca148106a287f651e56aac3ef55ec0cd9b20737
Primoz Stopar <primoz.stopar@student.um.si>
2019-05-17 21:26:46 +0200

    Dodan komentar, glede View-a ki ga je treba narediti za query

--- src/main/java/si/feri/pkm/optitech/Database/SQLCarsDatabase.java
+++ src/main/java/si/feri/pkm/optitech/Database/SQLCarsDatabase.java
@@ -17,6 +17,8 @@ public class SQLCarsDatabase {
              Statement statement = connection.createStatement()) {
 
             // Create and execute a SELECT SQL statement.
+            //DA TA QUERY DELUJE JE POTREBNO NAREDITI VIEW S TO KODO:
+            //Create view nekaj as (select Distinct carModelId from OptiTech.biz.Vehicles Where vehicleId IN  (select DISTINCT vehicleId from optitech.tlm.DriveData))
             String selectSql = "select * from (select * from optitech.reg.carModels where carModelId IN (select * from OptiTech.dbo.nekaj)) as prvi left join (select carModelId,countryId,carMaker from optitech.reg.carModels LEFT JOIN optitech.reg.CarMakers on optitech.reg.CarMakers.carMakerId = optitech.reg.CarModels.carMakerId) as drugi on prvi.carModelId = drugi.carModelId;";
             resultSet = statement.executeQuery(selectSql);
 
