5351c79917b3a73b293df2e398d68201ff4c28db
Primoz Stopar <primoz.stopar@student.um.si>
2019-06-12 16:39:56 +0200

    Trip Data backend

--- src/main/java/si/feri/pkm/optitech/Database/SQLTripData.java
+++ src/main/java/si/feri/pkm/optitech/Database/SQLTripData.java
@@ -1,6 +1,7 @@
 package si.feri.pkm.optitech.Database;
 
 import org.json.JSONObject;
+import si.feri.pkm.optitech.Entity.TripData;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -57,19 +58,40 @@ public class SQLTripData {
 
     }
 
-    public static void neKliciTega(int carId, String date) {
+    public static TripData neKliciTega(int carId, String date) {
         ResultSet resultSet;
+        TripData td = new TripData();
 
         try (Connection connection = DriverManager.getConnection(connectionUrl);
              Statement statement = connection.createStatement()) {
-            String selectSql = "select Convert(varchar(11), dateMsg, 23), MIN(dateMsg) as firstTime, MAX(dateMsg) as lastTime, MAX(userID) as userID, MAX(DrvAbsDistOdo) as lastDistance, MIN(DrvAbsDistOdo) as firstDistance, AVG(ScorePPAL) as scorePPAL, AVG(ScoreRPOL) as scoreRPOL, AVG(ScoreRVAR) as scoreRVAR, AVG(ScoreTotal) as scoreTotal, AVG(RpmAvg) as rpmAvg, MAX(RpmMax) as rpmMax, AVG(VssAvg) as vssAvg, MAX(VssMax) as vssMax from OptiTech.tlm.driveData where vehicleId = "+carId+" AND Convert(varchar(11), dateMsg, 23) = \'"+date+"\' GROUP BY Convert(varchar(11), dateMsg, 23);";
+            String selectSql = "select MIN(Convert(varchar(8), dateMsg, 108)) as firstTime, MAX(Convert(varchar(8), dateMsg, 108)) as lastTime, MAX(userID) as userID, MAX(DrvAbsDistOdo) as lastDistance, MIN(DrvAbsDistOdo) as firstDistance, AVG(ScorePPAL) as scorePPAL, AVG(ScoreRPOL) as scoreRPOL, AVG(ScoreRVAR) as scoreRVAR, AVG(ScoreTotal) as scoreTotal, AVG(RpmAvg) as rpmAvg, MAX(RpmMax) as rpmMax, AVG(VssAvg) as vssAvg, MAX(VssMax) as vssMax from OptiTech.tlm.driveData where vehicleId = "+carId+" AND Convert(varchar(11), dateMsg, 23) = \'"+date+"\' GROUP BY Convert(varchar(11), dateMsg, 23);";
 
             resultSet = statement.executeQuery(selectSql);
-
+            int score =0;
+            int stevec =0;
             while (resultSet.next()) {
+                for(int i=6;i<10;i++){
+                    if(resultSet.getInt(i) != 0){
+                        score += resultSet.getInt(i);
+                        stevec+=1;
+                    }
+                }
+
+            td = new TripData(
+              resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getInt(3),
+                    ((resultSet.getInt(4)-resultSet.getInt(5))/1000),
+                    (score/stevec),
+                    resultSet.getInt(10),
+                    resultSet.getInt(11),
+                    resultSet.getInt(12),
+                    resultSet.getInt(13)
+            );
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return td;
     }
 }
---
+++ src/main/java/si/feri/pkm/optitech/Entity/TripData.java
@@ -0,0 +1,115 @@
+package si.feri.pkm.optitech.Entity;
+
+public class TripData {
+    String startTime;
+    String finishTime;
+    int userId;
+    double distance;
+    int totalScore;
+    int rpmAvg;
+    int rpmMax;
+    int vssAvg;
+    int vssMax;
+    public  TripData(){
+
+    }
+
+    public TripData(String startTime, String finishTime, int userId, int distance, int totalScore, int rpmAvg, int rpmMax, int vssAvg, int vssMax) {
+        this.startTime = startTime;
+        this.finishTime = finishTime;
+        this.userId = userId;
+        this.distance = distance;
+        this.totalScore = totalScore;
+        this.rpmAvg = rpmAvg;
+        this.rpmMax = rpmMax;
+        this.vssAvg = vssAvg;
+        this.vssMax = vssMax;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(String finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public int getRpmAvg() {
+        return rpmAvg;
+    }
+
+    public void setRpmAvg(int rpmAvg) {
+        this.rpmAvg = rpmAvg;
+    }
+
+    public int getRpmMax() {
+        return rpmMax;
+    }
+
+    public void setRpmMax(int rpmMax) {
+        this.rpmMax = rpmMax;
+    }
+
+    public int getVssAvg() {
+        return vssAvg;
+    }
+
+    public void setVssAvg(int vssAvg) {
+        this.vssAvg = vssAvg;
+    }
+
+    public int getVssMax() {
+        return vssMax;
+    }
+
+    public void setVssMax(int vssMax) {
+        this.vssMax = vssMax;
+    }
+
+    @Override
+    public String toString() {
+        return "TripData{" +
+                "startTime='" + startTime + '\'' +
+                ", finishTime='" + finishTime + '\'' +
+                ", userId=" + userId +
+                ", distance=" + distance +
+                ", totalScore=" + totalScore +
+                ", rpmAvg=" + rpmAvg +
+                ", rpmMax=" + rpmMax +
+                ", vssAvg=" + vssAvg +
+                ", vssMax=" + vssMax +
+                '}';
+    }
+}
