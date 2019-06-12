cc623dcfad139fcf9ffea9a391cca8c817f46881
aljosasikosek <33753351+aljosasikosek@users.noreply.github.com>
2019-06-12 15:10:51 +0200

    Merge branch 'master' of C:\Users\Aljos\Faks\4. semester\PRA\OptiTech with conflicts.

--- src/main/java/si/feri/pkm/optitech/Database/SQLDatabaseConnection.java
+++ src/main/java/si/feri/pkm/optitech/Database/SQLDatabaseConnection.java
@@ -1,9 +1,6 @@
 package si.feri.pkm.optitech.Database;
 
 public class SQLDatabaseConnection {
-    static String connectionUrl =
-            "jdbc:sqlserver://UROSLENOVO\\SQL:51307;"
-                    + "database=OptiTech;"
-                    + "user=sa;"
-                    + "password=T3st1ranj3123.;";
+    static String connectionUrl = "jdbc:sqlserver://DESKTOP-24FQM30:55791;databaseName=OptiTech;integratedSecurity=true";
+
 }
